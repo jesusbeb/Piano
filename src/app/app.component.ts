d6450c988be608cc83afeaee362abbed292097a9
jesusbeb <jesusbelb@gmail.com>
2023-05-05 17:31:08 -0600

    Piano finalmente emite sonidos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Piano';
+
+  /* metodo que recibe un parametro numero de tipo number y no regresa nada */
+  aplicarSonido(numero: number): void {
+    //Creamos una instancia de audio
+    const audio = new Audio();
+    //la ruta del archivo de audio que ejecutara "note#.wav". El # se concatena con el numero que
+    //se enviara del html segun en la linea que se de clic
+    audio.src ="../assets/sonidos/note" + numero + '.wav'; 
+    audio.load(); //carga el audio
+    audio.play(); //ejecuta el audio
+  }
 }
